da27094171af668196ff96a16beb2151ee9e9822
westh <sh.han055@gmail.com>
2024-09-17 00:06:36 +0900

    2024-09-17

--- javaScript/tab.js
+++ javaScript/tab.js
@@ -1,15 +1,8 @@
 document.querySelector('.list').addEventListener('click', function(event) {
 
     if (event.target.classList.contains('tab-button')) {
-        let index = 0;
-        
-        for (let i in document.querySelectorAll('.tab-button')) {
-            if (document.querySelectorAll('.tab-button')[i] === event.target) {
-                index = i;
-                break;
-            }
-        }
-        
+        let index = event.target.dataset.id;
+                
         for (let el of document.querySelectorAll('.tab-button')) {
             el.classList.remove('orange');
         }
@@ -17,10 +10,9 @@ document.querySelector('.list').addEventListener('click', function(event) {
             el.classList.remove('show');
         }
         
-        console.log(index);
         document.querySelector('.list').children[index].classList.add('orange');
         document.querySelectorAll('.tab-content')[index].classList.add('show');
     } else {
-        alert('no');
+        event.stopPropagation();
     }
 });
